b7c80229d7981736ffeb1cea060f3428e7590b40
Tejas Parikh <tejas@tejasparikh.com>
2017-09-19 19:58:08 -0400

    Add Spring Session & Security
    Added HTTP Basic user authentication using in memory database (H2) and
    place holder configuration for MySQL JDBC datasource.

---
+++ src/main/java/com/csye6225/demo/auth/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.csye6225.demo.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
+import org.springframework.session.web.http.HttpSessionStrategy;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .authorizeRequests()
+        .antMatchers("/").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .httpBasic();
+  }
+
+  @Bean
+  public HttpSessionStrategy httpSessionStrategy() {
+    return new HeaderHttpSessionStrategy();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+    manager.createUser(User.withUsername("user").password("password").roles("USER").build());
+    return manager;
+  }
+}
