e529034e5b7f6fbee78a6df8c3e3bbe901553c35
Tejas Parikh <tejas@tejasparikh.com>
2017-09-19 19:57:33 -0400

    Add Spring Session & Security
    Added HTTP Basic user authentication using in memory database (H2) and
    place holder configuration for MySQL JDBC datasource.

--- src/main/java/com/csye6225/demo/controllers/HomeController.java
+++ src/main/java/com/csye6225/demo/controllers/HomeController.java
@@ -1,14 +1,17 @@
 package com.csye6225.demo.controllers;
 
 
+import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.google.gson.JsonObject;
+import java.util.Date;
 
 @Controller
 public class HomeController {
@@ -20,7 +23,13 @@ public class HomeController {
   public String welcome() {
 
     JsonObject jsonObject = new JsonObject();
-    jsonObject.addProperty("message", "Hello CSYE 6225!!!");
+
+    if (SecurityContextHolder.getContext().getAuthentication() != null
+        && SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken) {
+      jsonObject.addProperty("message", "you are not logged in!!!");
+    } else {
+      jsonObject.addProperty("message", "you are logged in. current time is " + new Date().toString());
+    }
 
     return jsonObject.toString();
   }
