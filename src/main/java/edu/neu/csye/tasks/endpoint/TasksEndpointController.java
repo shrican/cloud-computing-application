b18040136181979cb4516d472a85e2441963bc28
shrikant <mudholkar.s@husky.neu.edu>
2017-10-12 00:08:45 -0400

    Task Endpoints

---
+++ src/main/java/edu/neu/csye/tasks/endpoint/TasksEndpointController.java
@@ -0,0 +1,31 @@
+/**
+ * Varsha Bhanushali, 001234580, bhanushali.v@husky.neu.edu
+ * Shrikant Mudholkar, 001284732, mudholkar.s@husky.neu.edu
+ * Rahul Chandra, 01225683, chandra.ra@husky.neu.edu
+ * Manish Patil, 001228956, patil.man@husky.neu.edu
+ **/
+
+package edu.neu.csye.tasks.endpoint;
+import edu.neu.csye.tasks.endpoint.model.Task;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class TasksEndpointController implements TasksEndpointRest{
+
+
+    @Override
+    public Task create(Task task){
+        return null;
+    }
+
+    @Override
+    public List<Task> get(){
+        return null;
+    }
+}
---
+++ src/main/java/edu/neu/csye/tasks/endpoint/model/Task.java
@@ -0,0 +1,32 @@
+/**
+ * Varsha Bhanushali, 001234580, bhanushali.v@husky.neu.edu
+ * Shrikant Mudholkar, 001284732, mudholkar.s@husky.neu.edu
+ * Rahul Chandra, 01225683, chandra.ra@husky.neu.edu
+ * Manish Patil, 001228956, patil.man@husky.neu.edu
+ **/
+
+package edu.neu.csye.tasks.endpoint.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.NotBlank;
+import java.io.Serializable;
+
+/**
+ * Defines the properties and basic validation for a request to create a task.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task implements Serializable{
+
+    private static final long serialVersionUID = -7588073602465822378L;
+
+    private String id;
+
+    @NotBlank(message = "DESCRIPTION CANT BE BLANK")
+    private String description;
+}
