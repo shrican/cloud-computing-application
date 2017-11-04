36840bd930c18865cfc1d0c9dafe86f0a90fd1fe
Manish Patil <patil.man@husky.neu.edu>
2017-11-04 13:58:42 -0400

    readme changes

--- restartTomcat8.sh
+++ restartTomcat8.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo systemctl stop tomcat8 
+cd ~
+sudo chmod 777 csye6225.log
 cd /var/lib/tomcat8/webapps
 sudo rm -rf ROOT
-sudo systemctl restart tomcat8 
+sudo systemctl start tomcat8
