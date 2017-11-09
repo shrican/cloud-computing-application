4cfecea9fd9e79394999bec4f3ecce1f8f4c4c6e
Varsha <bhanushali.v@husky.neu.edu>
2017-11-08 23:38:26 -0500

    Merge branch 'assignment7-lab' of https://github.com/shrican/csye6225-fall2017 into assignment7
    # Conflicts:
    #	src/main/java/edu/neu/csye/tasks/service/TasksService.java

---
+++ src/main/java/edu/neu/csye/useraccount/endpoint/PasswordResetEndpointRest.java
@@ -0,0 +1,28 @@
+
+/**
+ * Varsha Bhanushali, 001234580, bhanushali.v@husky.neu.edu
+ * Shrikant Mudholkar, 001284732, mudholkar.s@husky.neu.edu
+ * Rahul Chandra, 01225683, chandra.ra@husky.neu.edu
+ * Manish Patil, 001228956, patil.man@husky.neu.edu
+ **/
+package edu.neu.csye.useraccount.endpoint;
+
+import edu.neu.csye.tasks.dataaccess.model.ResetTokenEntity;
+import edu.neu.csye.useraccount.endpoint.model.PasswordResetToken;
+import edu.neu.csye.useraccount.endpoint.model.UserAccount;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/forgot-password")
+public interface PasswordResetEndpointRest {
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    Response sendResetToken(ResetTokenEntity passwordReset);
+}
